44859b3e374e1d837f24c612a0b365bb359854bd
Ryan Chang <ryanchang@ryanchang-macbookpro.home>
2018-06-01 22:30:58 -0700

    starts on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import styled, { css } from 'react-emotion'
 // import Link from 'gatsby-link'
 
+export const HeaderContainer = styled('header')`
+  position: ${(props) => {
+    switch (props.level) {
+      case 'top':
+        return 'relative'
+      case 'scrolled':
+        return 'fixed'
+    }
+  }};
+  top: 0;
+  width: 100%;
+`
+export const header = css`
+  display: flex;
+  justify-content: space-between;
+  margin: 0.5em;
+`
+
+export const Hamburger = styled('button')`
+  
+`
+
 const Header = ({ siteTitle }) => (
-  <header>
-    <nav>
-      Hello
-    </nav>
-  </header>
+  <HeaderContainer level='top'>
+    <div className={header}>
+      <div>Hamburger</div>
+      <div>SANDALBOYZ</div>
+      <div>Bag</div>
+    </div>
+  </HeaderContainer>
 )
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { css } from 'react-emotion'
 // import Link from 'gatsby-link'
 
+const className = css({
+  height: '1000px',
+  backgroundColor: 'lightgray'
+})
+
 const IndexPage = () => (
-  <div>
+  <div className={className}>
     <h1>Hi people</h1>
     <p>Welcome to the new SANDALBOYZ storefront.</p>
     <p>It's still under construction if you couldn't tell.</p>
