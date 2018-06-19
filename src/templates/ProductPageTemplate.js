456d84cd387c15ba8ff093b2491b84386302cfe3
Ryan Chang <ryanchang@RyanChang-MacBookPro.local>
2018-06-18 22:40:06 -0700

    details on product page template

--- src/templates/ProductPageTemplate.js
+++ src/templates/ProductPageTemplate.js
@@ -1,22 +1,30 @@
 // @flow
 import React from 'react'
 import styled from 'react-emotion'
+import { get } from 'lodash'
 import { truncatePrice } from 'src/helpers'
 // Components
 import Select from 'react-select'
 // Styles
 import { gray, grayLight, grayDark } from 'src/theme'
 
+export const ProductTitleContainer = styled('div')`
+  margin-bottom: 15px;
+`
+
 export const ProductTitle = styled('h3')`
   margin: 0;
 `
 
-const selectStyles = {
+const selectStyles = injectedData => ({
+  container: () => ({
+    width: '60%',
+    height: '30px'
+  }),
   control: (base, { isFocused }) => ({
     display: 'flex',
     backgroundColor: 'white',
     height: '30px',
-    width: '200px',
     border: '1px solid',
     borderColor: isFocused ? 'black' : grayLight,
     borderRadius: 0,
@@ -24,18 +32,40 @@ const selectStyles = {
     boxShadow: 'gray'
   }),
   menu: () => ({
-    width: '200px',
+    background: 'white',
+    position: 'absolute',
+    width: `${injectedData.selectControlWidth}px`,
+    height: '200px',
     border: '1px solid black',
-    borderTop: 0
+    borderTop: 0,
+    overflowY: 'scroll'
   }),
   option: (_, { isDisabled, isFocused, isSelected }) => ({
     backgroundColor: isDisabled
       ? null
       : isSelected ? gray : isFocused ? grayLight : null,
-    padding: '0 5px',
+    padding: '2px 6px',
     color: isDisabled ? grayLight : 'black'
   })
-}
+})
+
+export const AddToCartContainer = styled('div')`
+  display: flex;
+  margin-bottom: 30px;
+`
+
+export const AddToCartButton = styled('button')`
+  background-color: black;
+  color: white;
+  border: 1px solid black;
+  border-left: 0;
+  height: 30px;
+  width: 40%;
+`
+
+export const ProductDescription = styled('article')`
+  padding-left: 1em;
+`
 
 // `pathContext` comes from `gatsby-node.js`.
 type PathContextType ={
@@ -51,6 +81,10 @@ type ProductPageTemplateProps = {
 }
 
 class ProductPageTemplate extends React.Component<ProductPageTemplateProps> {
+  selectControlWidth = () => (
+    get(document.getElementsByClassName('react-select__control'), '[0].offsetWidth')
+  )
+
   render () {
     // TODO: Have dynamic currency.
     const currency = 'USD'
@@ -64,20 +98,28 @@ class ProductPageTemplate extends React.Component<ProductPageTemplateProps> {
           ))
         }
         <div>
-          <ProductTitle>{pathContext.title}</ProductTitle>
-          <span>{truncatePrice(pathContext.variants[0].price)} {currency}</span>
-          <div>
+          <ProductTitleContainer>
+            <ProductTitle>{pathContext.title}</ProductTitle>
+            <span>{truncatePrice(pathContext.variants[0].price)} {currency}</span>
+          </ProductTitleContainer>
+          <AddToCartContainer>
             <Select
               options={pathContext.variants.map(variant => ({
                 value: variant.id,
                 label: variant.title,
                 isDisabled: !variant.availableForSale
               }))}
+              // NOTE: `classNamePrefix` for debugging.
+              classNamePrefix='react-select'
               // menuIsOpen
               isSearchable={false}
-              styles={selectStyles}
+              styles={selectStyles({
+                selectControlWidth: this.selectControlWidth()
+              })}
             />
-          </div>
+            <AddToCartButton>Add To Cart</AddToCartButton>
+          </AddToCartContainer>
+          <ProductDescription>{pathContext.description}</ProductDescription>
         </div>
       </div>
     )
