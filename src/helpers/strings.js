44af34d4c4f4a477b324f378d79a4fcce7e05785
Ryan Chang <ryanchang@RyanChang-MacBookPro.local>
2018-06-19 21:56:02 -0700

    adds comments

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,7 +1,14 @@
 // @flow
 
-type NodeTypeType = 'Product' | 'ProductVariant'
+// NOTE: These come from `gatsby-source-shopify` plugin.
+type NodeTypeType =
+  'Product' |
+  'ProductVariant' |
+  'ProductOption' |
+  'Article'
 
+// This helper function replaces the `Shopify__ProductVariant__` that gets
+// prepended by `gatsby-source-shopify`.
 export const extractIdFromGatsbyId = (string: string, nodeType: NodeTypeType) => (
   string.replace(`Shopify__${nodeType}__`, '')
 )
