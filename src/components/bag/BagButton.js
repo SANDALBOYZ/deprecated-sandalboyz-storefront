f2af09ecc2cdc47816947a80a6ff9fd040ce9c38
Ryan Chang <ryanchang@RyanChang-MacBookPro.local>
2018-06-19 22:40:16 -0700

    simplifies bag button

--- src/components/bag/BagButton.js
+++ src/components/bag/BagButton.js
@@ -1,13 +1,35 @@
 // @flow
 import React from 'react'
-import styled from 'react-emotion'
+import styled, { css, keyframes } from 'react-emotion'
 import { get } from 'lodash'
 import { Context } from 'src/layouts/index'
+
+export const bagPop = keyframes`
+  0% {
+    width: 25px;
+    height: 25px;
+  }
+
+  80% {
+    width: 28px;
+    height: 28px;
+  }
+
+  100% {
+    width: 25px;
+    height: 25px;
+  }
+`
+
+export const addedToCartStyle = css`
+  animation: ${bagPop} 0.5s ease-in-out;
+`
 
 export const BagButtonContainer = styled('button')`
   display: flex;
   justify-content: center;
   align-items: center;
+  align-content: center;
   width: 25px;
   height: 25px;
   background-color: ${({ theme, menuOpen }) => {
@@ -19,11 +41,8 @@ export const BagButtonContainer = styled('button')`
   border: 0;
   outline: 0;
   transition: background-color 2s ease;
-`
-
-export const BagCount = styled('span')`
-  color: #FFFFFF;
-  line-height: 24px;
+  color: white;
+  line-height: 25px;
 `
 
 const calculateQuantity = (edges: Array<Object>): number => (
@@ -33,14 +52,32 @@ const calculateQuantity = (edges: Array<Object>): number => (
   }, 0)
 )
 
-const BagButton = () => (
-  <Context.Consumer>
-    {context => (
-      <BagButtonContainer onClick={context.toggleBag} menuOpen={context.menuOpen}>
-        <BagCount>{calculateQuantity(get(context.checkout, 'lineItems.edges') || [])}</BagCount>
+class BagButton extends React.Component {
+  state = {
+    addedToCart: false
+  }
+
+  addedToBagCallback = () => {
+  }
+
+  render () {
+    const { context } = this.props
+
+    return (
+      <BagButtonContainer
+        onClick={context.toggleBag}
+        menuOpen={context.menuOpen}
+        className={this.state.addedToCart ? addedToCartStyle : undefined}
+        onAnimationEnd={() => this.setState({ addedToCart: false })}
+      >
+        {calculateQuantity(get(context.checkout, 'lineItems.edges') || [])}
       </BagButtonContainer>
-    )}
+    )
+  }
+}
+
+export default props => (
+  <Context.Consumer>
+    {context => <BagButton {...props} context={context} />}
   </Context.Consumer>
 )
-
-export default BagButton
